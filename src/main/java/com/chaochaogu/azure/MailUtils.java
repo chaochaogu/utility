00423f378fe7f55e0375c21045cf27a183b95c8f
chaochao.gu <723454695@qq.com>
2020-01-15 14:11:27 +0800

    update

--- src/main/java/com/chaochaogu/azure/MailUtils.java
+++ src/main/java/com/chaochaogu/azure/MailUtils.java
@@ -1,6 +1,5 @@
 package com.chaochaogu.azure;
 
-import com.google.common.base.Throwables;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -16,7 +15,7 @@ import javax.mail.internet.MimeMessage;
 @Slf4j
 public class MailUtils {
 
-    public static int sendMail(MailParams mailParams) {
+    public static int sendMail(MailParams mailParams) throws Exception {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         // javaMailSender.setHost("smtp.163.com");
         // javaMailSender.setHost("mail.pousheng.com");
@@ -26,21 +25,16 @@ public class MailUtils {
         //创建message
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
-        try {
-            //发件人
-            helper.setFrom(mailParams.getFrom());
-            //收件人
-            helper.setTo(mailParams.getTo());
-            //邮件标题
-            helper.setSubject(mailParams.getSubject());
-            //true指的是html邮件
-            helper.setText(mailParams.getContent(), mailParams.isHtml());
-            //发送邮件
-            javaMailSender.send(message);
-        } catch (Exception e) {
-            log.error("send mail error, cause {}", Throwables.getStackTraceAsString(e));
-            return -1;
-        }
+        //发件人
+        helper.setFrom(mailParams.getFrom());
+        //收件人
+        helper.setTo(mailParams.getTo());
+        //邮件标题
+        helper.setSubject(mailParams.getSubject());
+        //true指的是html邮件
+        helper.setText(mailParams.getContent(), mailParams.isHtml());
+        //发送邮件
+        javaMailSender.send(message);
         return 1;
     }
 }
